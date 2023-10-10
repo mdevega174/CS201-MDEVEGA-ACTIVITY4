0d384803c028960ae2bc967e1fb6f5bc3256fda8
mdevega174 <147475153+mdevega174@users.noreply.github.com>
2023-10-10 13:44:43 +0800

    Add files via upload

---
+++ CS201-MDEVEGA ACTIVITY4/script.js
@@ -0,0 +1,15 @@
+let fruit = "banana";
+
+switch (fruit){
+    case "Banana":
+        console.log("Banana is good!");
+        break;
+    case "Orange":
+        console.log("I am not a fan of orange.");
+        break;
+    case "Apple":
+        console.log("How you like them apples?");
+        break;
+    default:
+        console.log("I have never heard of that fruit.");
+}
